ece9a49a519305937f09b37f1246c6e7fbdfd980
ChaoTzuJung <s110319022@stu.ntue.edu.tw>
2020-07-12 16:22:40 +0800

    feat: Complete quiz and question logic.

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { Difficulty, QuestionState, fetchQuizQuestions } from './api';
 type AnswerObject = {
   question: string;
   answer: string;
-  correct: string;
+  correct: boolean;
   correctAnswer: string;
 }
 
@@ -44,8 +44,34 @@ const App = () => {
       setNumber(0);
       setLoading(false);
   };
-  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {};
-  const nextQuestuin = () => {}
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
+    if(!gameOver) {
+      // User answer
+      const answer = e.currentTarget.value
+      // Check answer against correct answer.
+      const correct = questions[number].correct_answer === answer;
+      // Add score if answer is correct
+      if(correct) setScore(prev => prev + 1);
+      // save awsner in the array for user answers
+      const answerObject = {
+        question: questions[number].question,
+        answer,
+        correct,
+        correctAnswer:  questions[number].correct_answer,
+      }
+      setUserAnswers(prev => [...prev, answerObject])
+
+    }
+  };
+  const nextQuestion = () => {
+    // Move on to the next question if not the last question
+    const nextQuestion = number + 1;
+    if(nextQuestion === TOTAL_QUESTIONS) {
+      setGameOver(true)
+    } else {
+      setNumber(nextQuestion);
+    }
+  }
 
   return (
     <div className="App">
@@ -70,7 +96,7 @@ const App = () => {
         !loading &&
         userAnswers.length === number + 1 &&
         number + 1 !== TOTAL_QUESTIONS ? (
-          <button className="start" onClick={nextQuestuin}>Next Question</button>
+          <button className="start" onClick={nextQuestion}>Next Question</button>
         ) : null
       }
     </div>
